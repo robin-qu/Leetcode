5d08e5b9d11edc525288cd72c95e9b0e5c44a5a7
robin-qu <hongbinqu9@gmail.com>
2019-04-07 17:29:28 -0700

    binary tree practice

---
+++ BinaryTree/110_BalancedBinaryTree.java
@@ -0,0 +1,67 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+// // Divide and Conquer
+// class ResultType {
+//     public int height;
+//     public boolean isBalanced;
+    
+//     public ResultType(int height, boolean isBalanced) {
+//         this.height = height;
+//         this.isBalanced = isBalanced;
+//     }
+// }
+
+// class Solution {
+//     public boolean isBalanced(TreeNode root) {
+//         return helper(root).isBalanced;   
+//     }
+    
+//     public ResultType helper(TreeNode root) {
+//         if (root == null) {
+//             return new ResultType(0, true);
+//         }
+        
+//         ResultType left = helper(root.left);
+//         ResultType right = helper(root.right);
+        
+//         if (!left.isBalanced || !right.isBalanced) {
+//             return new ResultType(-1, false);
+//         }
+        
+//         if (Math.abs(left.height - right.height) > 1) {
+//             return new ResultType(-1, false);
+//         }
+        
+//         return new ResultType(1 + Math.max(left.height, right.height), true);
+//     }
+// }
+
+// Traversal
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        
+        boolean condition1 = Math.abs(getHeight(root.left) - getHeight(root.right)) < 2;
+        boolean condition2 = isBalanced(root.left);
+        boolean condition3 = isBalanced(root.right);
+        
+        return condition1 && condition2 && condition3;
+    }
+    
+    private int getHeight(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        
+        return 1 + Math.max(getHeight(node.left), getHeight(node.right));
+    }
+}
---
+++ BinaryTree/LintCode596_MinimumSubtree.java
@@ -0,0 +1,193 @@
+/**
+ * Given a binary tree, find the subtree with minimum sum. Return the root of the subtree.
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+// // Traversal
+// public class Solution {
+//     /**
+//      * @param root: the root of binary tree
+//      * @return: the root of the minimum subtree
+//      */
+     
+//     private TreeNode res = null;
+//     private int min = Integer.MAX_VALUE;
+    
+//     public TreeNode findSubtree(TreeNode root) {
+//         helper(root);
+//         return res;
+//     }
+    
+//     private int helper(TreeNode root) {
+//         if (root == null) {
+//             return 0;
+//         }
+        
+//         int sum = root.val + helper(root.left) + helper(root.right);
+        
+//         if (sum < min) {
+//             min = sum;
+//             res = root;
+//         }
+        
+//         return sum;
+//     }
+// }
+
+
+// Divide and Conquer
+class ResultType {
+    public TreeNode root;
+    public int sum;
+    public int min;
+    
+    public ResultType(TreeNode root, int sum, int min) {
+        this.root = root;
+        this.sum = sum;
+        this.min = min;
+    }
+}
+
+public class Solution {
+    /**
+     * @param root: the root of binary tree
+     * @return: the root of the minimum subtree
+     */
+    public TreeNode findSubtree(TreeNode root) {
+        return helper(root).root;
+    }
+    
+    private ResultType helper(TreeNode root) {
+        if (root == null) {
+            return new ResultType(null, 0, Integer.MAX_VALUE);
+        }
+        
+        ResultType left = helper(root.left);
+        ResultType right = helper(root.right);
+        
+        ResultType result = new ResultType(root,
+                                root.val + left.sum + right.sum,
+                                root.val + left.sum + right.sum);
+                                
+        if (left.min < result.min) {
+            result.min = left.min;
+            result.root = left.root;
+        }
+        
+        if (right.min < result.min) {
+            result.min = right.min;
+            result.root = right.root;
+        }
+        
+        return result;
+    }
+}
+
+// // DFS Iterative
+// public class Solution {
+//     /**
+//      * @param root: the root of binary tree
+//      * @return: the root of the minimum subtree
+//      */
+//     public TreeNode findSubtree(TreeNode root) {
+//         Stack<TreeNode> stack = new Stack<>();
+//         Stack<Integer> sums = new Stack<>();
+//         int min = Integer.MAX_VALUE;
+//         TreeNode res = null;
+        
+//         if (root == null) {
+//             return res;
+//         }
+        
+//         stack.push(root);
+//         sums.push(getSum(root));
+        
+//         while (!stack.isEmpty()) {
+//             TreeNode curr = stack.pop();
+//             int sum = sums.pop();
+            
+//             if (sum < min) {
+//                 res = curr;
+//             }
+            
+//             if (curr.left != null) {
+//                 stack.push(curr.left);
+//                 sums.push(getSum(curr.left));
+//             }
+            
+//             if (curr.right != null) {
+//                 stack.push(curr.right);
+//                 sums.push(getSum(curr.right));
+//             }
+//         }
+        
+//         return res;
+//     }
+    
+//     private int getSum(TreeNode root) {
+//         if (root == null) {
+//             return 0;
+//         }
+        
+//         return root.val + getSum(root.left) + getSum(root.right);
+//     }
+// }
+
+
+// // BFS Iterative
+// public class Solution {
+//     /**
+//      * @param root: the root of binary tree
+//      * @return: the root of the minimum subtree
+//      */
+//     public TreeNode findSubtree(TreeNode root) {
+//         Queue<TreeNode> queue = new LinkedList<>();
+//         Queue<Integer> sums = new LinkedList<>();
+//         int min = Integer.MAX_VALUE;
+//         TreeNode res = null;
+        
+//         if (root == null) {
+//             return res;
+//         }
+        
+//         queue.add(root);
+//         sums.add(getSum(root));
+        
+//         while (!queue.isEmpty()) {
+//             TreeNode curr = queue.remove();
+//             int sum = sums.remove();
+            
+//             if (sum < min) {
+//                 res = curr;
+//             }
+            
+//             if (curr.left != null) {
+//                 queue.add(curr.left);
+//                 sums.add(getSum(curr.left));
+//             }
+            
+//             if (curr.right != null) {
+//                 queue.add(curr.right);
+//                 sums.add(getSum(curr.right));
+//             }
+//         }
+        
+//         return res;
+//     }
+    
+//     private int getSum(TreeNode root) {
+//         if (root == null) {
+//             return 0;
+//         }
+        
+//         return root.val + getSum(root.left) + getSum(root.right);
+//     }
+// }
