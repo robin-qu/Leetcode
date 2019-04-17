fa3441cae346a3cf8c536428a3d772140326d977
robin-qu <hongbinqu9@gmail.com>
2019-04-17 10:09:16 -0700

    BFS practice

---
+++ BreadthFirstSearch/LintCode127_TopologicalSorting.java
@@ -0,0 +1,65 @@
+/**
+ * Definition for Directed graph.
+ * class DirectedGraphNode {
+ *     int label;
+ *     ArrayList<DirectedGraphNode> neighbors;
+ *     DirectedGraphNode(int x) { label = x; neighbors = new ArrayList<DirectedGraphNode>(); }
+ * };
+ */
+
+public class Solution {
+    /*
+     * @param graph: A list of Directed graph node
+     * @return: Any topological order for the given graph.
+     */
+    public ArrayList<DirectedGraphNode> topSort(ArrayList<DirectedGraphNode> graph) {
+        ArrayList<DirectedGraphNode> res = new ArrayList<>();
+        
+        if (graph == null) {
+            return res;
+        }
+        
+        Map<DirectedGraphNode, Integer> indegree = getIndegree(graph);
+        Queue<DirectedGraphNode> queue = new LinkedList<>();
+        
+        for (DirectedGraphNode node : graph) {
+            if (indegree.get(node) == 0) {
+                queue.add(node);
+            }
+        }
+        
+        while (!queue.isEmpty()) {
+            DirectedGraphNode curr = queue.remove();
+            res.add(curr);
+            
+            for (DirectedGraphNode neighbor : curr.neighbors) {
+                indegree.put(neighbor, indegree.get(neighbor) - 1);
+                if (indegree.get(neighbor) == 0) {
+                    queue.add(neighbor);
+                }
+            }
+        }
+        
+        return res;
+    }
+    
+    private Map<DirectedGraphNode, Integer> getIndegree(List<DirectedGraphNode> graph) {
+        Map<DirectedGraphNode, Integer> indegree = new HashMap<>();
+        
+        for (DirectedGraphNode node : graph) {
+            if (!indegree.containsKey(node)) {
+                indegree.put(node, 0);
+            }
+            
+            for (DirectedGraphNode neighbor : node.neighbors) {
+                if (!indegree.containsKey(neighbor)) {
+                    indegree.put(neighbor, 1);
+                } else {
+                    indegree.put(neighbor, indegree.get(neighbor) + 1);
+                }
+            }
+        }
+        
+        return indegree;
+    }
+}
