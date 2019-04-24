ed6cbb2696d36f00f39bc479015f05e3612798a5
robin-qu <hongbinqu9@gmail.com>
2019-04-24 15:59:11 -0700

    DFS practice

--- DepthFirstSearch/46_Permutations.java
+++ DepthFirstSearch/46_Permutations.java
@@ -8,6 +8,7 @@
         
 //         if (nums.length == 0) {
 //             res.add(new ArrayList<>());
+//             return res;
 //         }
         
 //         List<Integer> permutation = new ArrayList<>();
@@ -36,6 +37,50 @@
 //     }
 // }
 
+// class Solution {
+//     public List<List<Integer>> permute(int[] nums) {
+//         List<List<Integer>> res = new ArrayList<>();
+        
+//         if (nums == null) {
+//             return res;
+//         }
+        
+//         if (nums.length == 0) {
+//             res.add(new ArrayList<>());
+//         }
+        
+//         List<Integer> permutation = new ArrayList<>();
+//         boolean[] visited = new boolean[nums.length];
+        
+//         helper(nums, visited, permutation, res);
+        
+//         return res;
+//     }
+    
+//     private void helper(int[] nums, 
+//                         boolean[] visited,
+//                         List<Integer> permutation, 
+//                         List<List<Integer>> res) {
+//         if (permutation.size() == nums.length) {
+//             res.add(new ArrayList<>(permutation));
+//             return;
+//         }
+        
+//         for (int i = 0; i < nums.length; i++) {
+//             if (visited[i]) {
+//                 continue;
+//             }
+//             permutation.add(nums[i]);
+//             visited[i] = true;
+//             helper(nums, visited, permutation, res);
+//             permutation.remove(permutation.size() - 1);
+//             visited[i] = false;
+//         }
+//     }
+// }
+
+
+// Iterative
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
@@ -48,32 +93,55 @@ class Solution {
             res.add(new ArrayList<>());
         }
         
-        List<Integer> permutation = new ArrayList<>();
-        boolean[] visited = new boolean[nums.length];
+        Arrays.sort(nums);
+        List<Integer> permutation = arrayToList(nums);
         
-        helper(nums, visited, permutation, res);
+        while (!res.contains(permutation)) {
+            res.add(permutation);
+            nextPermutation(nums);
+            permutation = arrayToList(nums);
+        }
         
         return res;
     }
     
-    private void helper(int[] nums, 
-                        boolean[] visited,
-                        List<Integer> permutation, 
-                        List<List<Integer>> res) {
-        if (permutation.size() == nums.length) {
-            res.add(new ArrayList<>(permutation));
+    private void nextPermutation(int[] nums) {
+        List<Integer> nextPermutation = new ArrayList<>();
+        
+        if (nums == null || nums.length == 0) {
             return;
         }
         
-        for (int i = 0; i < nums.length; i++) {
-            if (visited[i]) {
-                continue;
-            }
-            permutation.add(nums[i]);
-            visited[i] = true;
-            helper(nums, visited, permutation, res);
-            permutation.remove(permutation.size() - 1);
-            visited[i] = false;
+        // Finds the last ascending position
+        int idx = nums.length - 1;
+        while (idx > 0 && nums[idx - 1] >= nums[idx]) {
+            idx--;
         }
+        
+        if (idx == 0) {
+            Arrays.sort(nums);
+            return;
+        }
+        
+        // Finds the first bigger value
+        int i = nums.length - 1;
+        while (i >= idx && nums[idx - 1] >= nums[i]) {
+            i--;
+        }
+        
+        int temp = nums[i];
+        nums[i] =  nums[idx - 1];
+        nums[idx - 1] = temp;
+        Arrays.sort(nums, idx, nums.length);;
+    }
+    
+    private List<Integer> arrayToList(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        
+        for (int num : nums) {
+            list.add(num);
+        }
+        
+        return list;
     }
 }
