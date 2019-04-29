4c48ad0db794616ec19c29a5259bb61ef2ff9e98
robin-qu <hongbinqu9@gmail.com>
2019-04-29 09:27:22 -0700

    linked list practice

--- LinkedList/148_SortList.java
+++ LinkedList/148_SortList.java
@@ -7,7 +7,70 @@
  * }
  */
 
-// Quick sort
+// // Quick sort
+// class Solution {
+//     public ListNode sortList(ListNode head) {
+//         if (head == null || head.next == null) {
+//             return head;
+//         }
+        
+//         ListNode pivot = head;
+        
+//         ListNode leftDummy = new ListNode(0);
+//         ListNode left = leftDummy;
+//         ListNode midDummy = new ListNode(0);
+//         ListNode middle = midDummy;
+//         ListNode rightDummy = new ListNode(0);
+//         ListNode right = rightDummy;
+        
+//         ListNode curr = head;
+//         while (curr != null) {
+//             if (curr.val < pivot.val) {
+//                 left.next = curr;
+//                 left = left.next;
+//             } else if (curr.val > pivot.val) {
+//                 right.next = curr;
+//                 right = right.next;
+//             } else {
+//                 middle.next = curr;
+//                 middle = middle.next;
+//             }
+//             curr = curr.next;
+//         }
+//         left.next = null;
+//         right.next = null;
+//         middle.next = null;
+        
+//         ListNode sortedLeft = sortList(leftDummy.next);
+//         ListNode sortedRight = sortList(rightDummy.next);
+        
+//         return merge(sortedLeft, midDummy.next, sortedRight);
+//     }
+    
+//     private ListNode merge(ListNode left, ListNode mid, ListNode right) {
+//         ListNode dummy = new ListNode(0);
+//         ListNode curr = dummy;
+//         while (left != null) {
+//             curr.next = left;
+//             curr = curr.next;
+//             left = left.next;
+//         }
+//         while (mid != null) {
+//             curr.next = mid;
+//             curr = curr.next;
+//             mid = mid.next;
+//         }
+//         while (right != null) {
+//             curr.next = right;
+//             curr = curr.next;
+//             right = right.next;
+//         }
+        
+//         return dummy.next;
+//     }
+// }
+
+// Merge sort
 class Solution {
     public ListNode sortList(ListNode head) {
         if (head == null || head.next == null) {
@@ -15,36 +78,42 @@ class Solution {
         }
         
         ListNode mid = getMid(head);
+        ListNode left = head;
+        ListNode right = mid.next;
+        mid.next = null;
         
-        ListNode leftDummy = new ListNode(0);
-        ListNode left = leftDummy;
-        ListNode midDummy = new ListNode(0);
-        ListNode middle = midDummy;
-        ListNode rightDummy = new ListNode(0);
-        ListNode right = rightDummy;
+        ListNode sortedLeft = sortList(left);
+        ListNode sortedRight = sortList(right);
         
-        ListNode curr = head;
-        while (curr != null) {
-            if (curr.val < mid.val) {
-                left.next = curr;
-                left = left.next;
-            } else if (curr.val > mid.val) {
-                right.next = curr;
-                right = right.next;
+        return mergeTwoSortedList(sortedLeft, sortedRight);
+    }
+    
+    private ListNode mergeTwoSortedList(ListNode left, ListNode right) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        ListNode leftCurr = left;
+        ListNode rightCurr = right;
+        
+        while (leftCurr != null && rightCurr != null) {
+            if (leftCurr.val < rightCurr.val) {
+                curr.next = leftCurr;
+                leftCurr = leftCurr.next;
             } else {
-                middle.next = curr;
-                middle = middle.next;
+                curr.next = rightCurr;
+                rightCurr = rightCurr.next;
             }
             curr = curr.next;
         }
-        left.next = null;
-        right.next = null;
-        middle.next = null;
         
-        ListNode sortedLeft = sortList(leftDummy.next);
-        ListNode sortedRight = sortList(rightDummy.next);
+        if (leftCurr != null) {
+            curr.next = leftCurr;
+        }
         
-        return merge(sortedLeft, midDummy.next, sortedRight);
+        if (rightCurr != null) {
+            curr.next = rightCurr;
+        }
+        
+        return dummy.next;
     }
     
     private ListNode getMid(ListNode head) {
@@ -57,27 +126,5 @@ class Solution {
         }
         
         return slow;
-    }
-    
-    private ListNode merge(ListNode left, ListNode mid, ListNode right) {
-        ListNode dummy = new ListNode(0);
-        ListNode curr = dummy;
-        while (left != null) {
-            curr.next = left;
-            curr = curr.next;
-            left = left.next;
-        }
-        while (mid != null) {
-            curr.next = mid;
-            curr = curr.next;
-            mid = mid.next;
-        }
-        while (right != null) {
-            curr.next = right;
-            curr = curr.next;
-            right = right.next;
-        }
-        
-        return dummy.next;
     }
 }
