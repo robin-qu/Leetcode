04a5860bd51e459085cfd3bf04f3c2db107ef2af
robin-qu <hongbinqu9@gmail.com>
2019-05-01 16:29:59 -0700

    two pointers practice

---
+++ TwoPointers/26_RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        
+        int left = 0;
+        
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] != nums[left]) {
+                left++;
+                nums[left] = nums[right];
+            }
+        }
+        
+        return left + 1;
+    }
+}
