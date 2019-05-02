291e8dc579f8faf3565346b30b8537ee6acb53fd
robin-qu <hongbinqu9@gmail.com>
2019-05-02 16:53:10 -0700

    two pointers practice

---
+++ TwoPointers/167_TwoSumIIInputArrayIsSorted.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        if (numbers == null || numbers.length == 0) {
+            return new int[] {};
+        }
+        
+        int left = 0;
+        int right = numbers.length - 1;
+               
+        while (left < right) {
+            if (numbers[left] + numbers[right] > target) {
+                right--;
+            } else if (numbers[left] + numbers[right] < target) {
+                left++;
+            } else {
+                return new int[] {left + 1, right + 1};
+            }
+        }
+        return new int[] {};
+    }
+}
