257fd05f3fb9a422e3c80b163150f35be900f7fc
robin-qu <hongbinqu9@gmail.com>
2019-05-02 11:13:30 -0700

    two pointers practice

--- TwoPointers/912_SortAnArray.java
+++ TwoPointers/912_SortAnArray.java
@@ -51,6 +51,65 @@
 //     }
 // }
 
+// // Quick Sort
+// class Solution {
+//     public int[] sortArray(int[] nums) {
+//         if (nums == null || nums.length == 0) {
+//             return nums;
+//         }
+        
+//         quickSort(nums, 0, nums.length - 1);
+//         return nums;
+//     }
+    
+//     private void quickSort(int[] nums, int left, int right) {
+//         if (left >= right) {
+//             return;
+//         }
+        
+//         int l = left;
+//         int r = right;
+//         int pivot = nums[left];
+        
+//         while (l < r) {
+//             while (l < r && nums[l] <= pivot) {
+//                 l++;
+//             }
+//             while (l < r && nums[r] > pivot) {
+//                 r--;
+//             }
+            
+//             if (l < r) {
+//                 swap(nums, l, r);
+//                 l++;
+//                 r--;
+//             }
+//         } // post: r(l)  or  r l
+        
+//         if (r == l) {
+//             if (nums[r] <= pivot) {
+//                 swap(nums, left, r);
+//                 quickSort(nums, left, r - 1);
+//                 quickSort(nums, r + 1, right);
+//             } else {
+//                 swap(nums, left, r - 1);
+//                 quickSort(nums, left, r - 2);
+//                 quickSort(nums, r, right);
+//             }
+//         } else {
+//             swap(nums, left, r);
+//             quickSort(nums, left, r - 1);
+//             quickSort(nums, l, right);
+//         }
+//     }
+    
+//     private void swap(int[] nums, int i, int j) {
+//         int temp = nums[i];
+//         nums[i] = nums[j];
+//         nums[j] = temp;
+//     }
+// }
+
 // Quick Sort
 class Solution {
     public int[] sortArray(int[] nums) {
@@ -69,38 +128,25 @@ class Solution {
         
         int l = left;
         int r = right;
-        int pivot = nums[left];
+        int pivot = nums[(left + right) / 2];
         
-        while (l < r) {
-            while (l < r && nums[l] <= pivot) {
+        while (l <= r) {
+            while (l <= r && nums[l] < pivot) {
                 l++;
             }
-            while (l < r && nums[r] > pivot) {
+            while (l <= r && nums[r] > pivot) {
                 r--;
             }
             
-            if (l < r) {
+            if (l <= r) {
                 swap(nums, l, r);
                 l++;
                 r--;
             }
-        } // post: r(l)  or  r l
+        } // post: r l  or  r x l
         
-        if (r == l) {
-            if (nums[r] <= pivot) {
-                swap(nums, left, r);
-                quickSort(nums, left, r - 1);
-                quickSort(nums, r + 1, right);
-            } else {
-                swap(nums, left, r - 1);
-                quickSort(nums, left, r - 2);
-                quickSort(nums, r, right);
-            }
-        } else {
-            swap(nums, left, r);
-            quickSort(nums, left, r - 1);
-            quickSort(nums, l, right);
-        }
+        quickSort(nums, left, r);
+        quickSort(nums, l, right);
     }
     
     private void swap(int[] nums, int i, int j) {
---
+++ TwoPointers/LintCode373_PartitionArrayByOddAndEven.java
@@ -0,0 +1,36 @@
+public class Solution {
+    /*
+     * @param nums: an array of integers
+     * @return: nothing
+     */
+    public void partitionArray(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+        
+        int left = 0;
+        int right = nums.length - 1;
+        
+        while (left < right) {
+            while (left < right && nums[left] % 2 == 1) {
+                left++;
+            }
+            
+            while (left < right && nums[right] % 2 == 0) {
+                right--;
+            }
+            
+            if (left < right) {
+                swap(nums, left, right);
+                left++;
+                right--;
+            }
+        }
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
