84968ad4f42f92d5dce3aee2b73606a8e16eb07d
robin-qu <hongbinqu9@gmail.com>
2019-05-04 22:01:07 -0700

    two pointers practice

--- TwoPointers/125_ValidPalindrome.java
+++ TwoPointers/125_ValidPalindrome.java
@@ -1,26 +1,3 @@
-// class Solution {
-//     public boolean isPalindrome(String s) {
-//         if (s.length() == 0) return true;
-//         s = s.toLowerCase();
-//         int front = 0;
-//         int back = s.length() - 1;
-//         while (front < back) {
-//             if (!isAlphanumeric(s.charAt(front))) front++;
-//             else if (!isAlphanumeric(s.charAt(back))) back--;
-//             else if (s.charAt(front) != s.charAt(back)) return false;
-//             else {
-//                 front++;
-//                 back--;
-//             }
-//         }
-//         return true;
-//     }
-//     private boolean isAlphanumeric(char c) {
-//         return (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9');
-//     }
-// }
-
-
 class Solution {
     public boolean isPalindrome(String s) {
         if (s.length() == 0) {
@@ -33,15 +10,15 @@ class Solution {
         int len = s.length();
         
         while (left < right) {
-            while (left < len && !isAlphanumeric(s.charAt(left))) {
+            while (left < right && !isAlphanumeric(s.charAt(left))) {
                 left++;
             }
             
-            while (right >= 0 && !isAlphanumeric(s.charAt(right))) {
+            while (left < right && !isAlphanumeric(s.charAt(right))) {
                 right--;
             }
             
-            if (left < len && right >= 0) {
+            if (left < right) {
                 if (s.charAt(left) != s.charAt(right)) {
                     return false;
                 }
