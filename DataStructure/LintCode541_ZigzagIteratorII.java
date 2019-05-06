db8ec6aae14061bcd5dd82355c88140254120426
robin-qu <hongbinqu9@gmail.com>
2019-05-06 13:56:23 -0700

    data structure practice

--- DataStructure/LintCode541_ZigzagIteratorII.java
+++ DataStructure/LintCode541_ZigzagIteratorII.java
@@ -1,47 +1,74 @@
-public class ZigzagIterator2 {
-    /*
-    * @param vecs: a list of 1d vectors
-    */
-    
-    private List<Iterator<Integer>> itrList;
-    private int selector;
-    
-    public ZigzagIterator2(List<List<Integer>> vecs) {
-        itrList = new ArrayList<>();
-        for (List<Integer> list : vecs) {
-            itrList.add(list.iterator());
-        }
-        selector = 0;
+// // Stack
+// public class Vector2D implements Iterator<Integer> {
+//     Stack<Object> stack = new Stack<>();
+
+//     public Vector2D(List<List<Integer>> vec2d) {
+//         for (int i = vec2d.size() - 1; i >= 0; i--) {
+//             stack.push(vec2d.get(i));
+//         }
+//     }
+
+//     @Override
+//     public Integer next() {
+//         return (int) stack.pop();
+//     }
+
+//     @Override
+//     public boolean hasNext() {
+//         while (!stack.isEmpty() && !(stack.peek() instanceof Integer)) {
+//             List<Integer> vec = (List<Integer>) stack.pop();
+//             for (int i = vec.size() - 1; i >= 0; i--) {
+//                 stack.push(vec.get(i));
+//             }
+//         }
+//         return !stack.isEmpty();
+//     }
+
+//     @Override
+//     public void remove() {
+//         stack.pop();
+//     }
+// }
+
+// Iterator
+public class Vector2D implements Iterator<Integer> {
+    private Iterator<Integer> intItr;
+    private Iterator<List<Integer>> listItr;
+
+    public Vector2D(List<List<Integer>> vec2d) {
+        listItr = vec2d.iterator();
+        intItr = null;
     }
 
-    /*
-     * @return: An integer
-     */
-    public int next() {
-        while (!itrList.get(selector % itrList.size()).hasNext()) {
-            selector++;
-        }
-        int res = itrList.get(selector % itrList.size()).next();
-        selector++;
-        return res;
+    @Override
+    public Integer next() {
+        return intItr.next();
     }
 
-    /*
-     * @return: True if has next
-     */
+    @Override
     public boolean hasNext() {
-        for (Iterator<Integer> itr : itrList) {
-            if (itr.hasNext()) {
+        if (intItr != null && intItr.hasNext()) {
+            return true;
+        }
+        while (listItr.hasNext()) {
+            List<Integer> vec = listItr.next();
+            intItr = vec.iterator();
+            if (intItr.hasNext()) {
                 return true;
             }
         }
         return false;
     }
+
+    @Override
+    public void remove() {
+        intItr.next();
+    }
 }
 
+
 /**
- * Your ZigzagIterator2 object will be instantiated and called as such:
- * ZigzagIterator2 solution = new ZigzagIterator2(vecs);
- * while (solution.hasNext()) result.add(solution.next());
- * Output result
+ * Your Vector2D object will be instantiated and called as such:
+ * Vector2D i = new Vector2D(vec2d);
+ * while (i.hasNext()) v[f()] = i.next();
  */
