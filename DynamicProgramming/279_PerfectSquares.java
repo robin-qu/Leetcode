b7bb0d8cbcdf3d874ef9be62e752b3caf8d1021a
robin-qu <hongbinqu9@gmail.com>
2019-05-14 13:21:58 -0700

    DP practice

---
+++ DynamicProgramming/279_PerfectSquares.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numSquares(int n) {
+        if (n <= 0) {
+            return 0;
+        }
+        
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        
+        for (int i = 1; i < n + 1; i++) {
+            dp[i] = i;
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], 1 + dp[i - j * j]);
+            }
+        }
+        
+        return dp[n];
+    }
+}
