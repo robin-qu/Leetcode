63487ad0b1ad58552057bb7503d70d1a37055409
robin-qu <hongbinqu9@gmail.com>
2019-05-22 09:56:20 -0700

    review

---
+++ AlgorithmReview/LintCode11_SearchRangeInBinarySearchTree.java
@@ -0,0 +1,45 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param root: param root: The root of the binary search tree
+     * @param k1: An integer
+     * @param k2: An integer
+     * @return: return: Return all keys that k1<=key<=k2 in ascending order
+     */
+    public List<Integer> searchRange(TreeNode root, int k1, int k2) {
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+        
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            
+            curr = stack.pop();
+            if (k1 <= curr.val && curr.val <= k2) {
+                res.add(curr.val);
+            }
+            
+            if (curr.val > k2) {
+                break;
+            }
+            
+            curr = curr.right;
+        }
+        
+        return res;
+    }
+}
