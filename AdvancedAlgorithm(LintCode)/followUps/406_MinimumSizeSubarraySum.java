ee6931d76c792c2c3d6a6caa6f6eeea714591c5b
robin-qu <hongbinqu9@gmail.com>
2019-06-02 14:24:15 -0700

    review:

--- AdvancedAlgorithm(LintCode)/followUps/406_MinimumSizeSubarraySum.java
+++ AdvancedAlgorithm(LintCode)/followUps/406_MinimumSizeSubarraySum.java
@@ -30,7 +30,39 @@
 //     }
 // }
 
-// Optimized by removing the prefixSum array
+// // Optimized by removing the prefixSum array
+// public class Solution {
+//     /**
+//      * @param nums: an array of integers
+//      * @param s: An integer
+//      * @return: an integer representing the minimum size of subarray
+//      */
+//     public int minimumSize(int[] nums, int s) {
+//         if (nums == null || nums.length == 0) {
+//             return -1;
+//         }
+        
+//         int n = nums.length;
+//         int sum = 0;
+        
+//         int minSize = Integer.MAX_VALUE;
+//         int left = 0;
+        
+//         for (int right = 0; right < n; right++) {
+//             sum += nums[right];
+//             while (sum >= s) {
+//                 minSize = Math.min(minSize, right - left + 1);
+//                 sum -= nums[left];
+//                 left++;
+//             }
+//         }
+        
+//         return minSize == Integer.MAX_VALUE ? -1 : minSize;
+//     }
+// }
+
+
+// Use left pointer as main pointer
 public class Solution {
     /**
      * @param nums: an array of integers
@@ -46,15 +78,19 @@ public class Solution {
         int sum = 0;
         
         int minSize = Integer.MAX_VALUE;
-        int left = 0;
+        int right = 0;
         
-        for (int right = 0; right < n; right++) {
-            sum += nums[right];
-            while (sum >= s) {
-                minSize = Math.min(minSize, right - left + 1);
-                sum -= nums[left];
-                left++;
+        for (int left = 0; left < n; left++) {
+            while (sum < s && right < n) {  // requires right < n to avoid out of bound exception
+                sum += nums[right];
+                right++;
             }
+            
+            if (sum >= s) {  // needs to check if sum is bigger than s, since right can >= n
+                minSize = Math.min(minSize, right - left);
+            }
+            
+            sum -= nums[left];
         }
         
         return minSize == Integer.MAX_VALUE ? -1 : minSize;
