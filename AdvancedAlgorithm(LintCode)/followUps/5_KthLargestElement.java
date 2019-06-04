3d6c10a524d4181a0f5a7bb8fb0b9c01895cb09d
robin-qu <hongbinqu9@gmail.com>
2019-06-03 17:33:27 -0700

    followUps

---
+++ AdvancedAlgorithm(LintCode)/followUps/5_KthLargestElement.java
@@ -0,0 +1,40 @@
+private int quickSelect(int k, int[] nums, int start, int end) {
+        if (start == end) {
+            return nums[start];
+        }
+        
+        int left = start;
+        int right = end;
+        int pivot = nums[start + (end - start) / 2];
+        
+        while (left <= right) {
+            while (left <= right && nums[left] > pivot) {
+                left++;
+            }
+            
+            while (left <= right && nums[right] < pivot) {
+                right--;
+            }
+            
+            if (left <= right) {
+                swap(nums, left, right);
+                left++;
+                right--;
+            }
+        }
+        
+        // start ... right  k - 1  left ... end
+        if (k - 1 <= right) {
+            return quickSelect(k, nums, start, right);
+        } else if (left <= k - 1) {
+            return quickSelect(k, nums, left, end);
+        } else {
+            return nums[k - 1];
+        }
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
