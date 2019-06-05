036cc43cb945e87028206c10551fbd38795e9148
robin-qu <hongbinqu9@gmail.com>
2019-06-05 09:56:37 -0700

    review

---
+++ AlgorithmReview/15_3Sum.java
@@ -0,0 +1,47 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums == null || nums.length < 3) {
+            return res;
+        }
+        
+        Arrays.sort(nums);
+        
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            twoSum(nums, i + 1, -nums[i], res);
+        }
+        
+        return res;
+    }
+    
+    private void twoSum(int[] nums, int start, int target, List<List<Integer>> res) {
+        
+        int left = start;
+        int right = nums.length - 1;
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            if (sum > target) {
+                right--;
+            } else if (sum < target) {
+                left++;
+            } else {
+                List<Integer> list = new ArrayList<>();
+                list.add(-target);
+                list.add(nums[left]);
+                list.add(nums[right]);
+                res.add(list);
+                left++;
+                right--;
+                while (left < right && nums[left] == nums[left - 1]) {
+                    left++;
+                }
+                while (left < right && nums[right] == nums[right + 1]) {
+                    right--;
+                }
+            }
+        }
+    }
+}
