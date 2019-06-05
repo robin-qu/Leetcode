b1b369324578db2b2899015c70cc732f708be9fc
robin-qu <hongbinqu9@gmail.com>
2019-06-05 09:02:52 -0700

    review

---
+++ AlgorithmReview/167_TwoSumIIInputArrayIsSorted.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        if (numbers == null || numbers.length == 0) {
+            return new int[]{};
+        }
+        
+        int left = 0;
+        int right = numbers.length - 1;
+        int sum;
+        
+        while (left < right) {
+            sum = numbers[left] + numbers[right];
+            
+            if (sum > target) {
+                right--;
+            } else if (sum < target) {
+                left++;
+            } else {
+                return new int[]{left + 1, right + 1};
+            }
+        }
+        
+        return new int[]{};
+    }
+}
