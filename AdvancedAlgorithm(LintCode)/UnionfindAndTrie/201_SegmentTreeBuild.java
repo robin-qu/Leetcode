f168fe977e1052c21909ea2c4819604b090503ca
robin-qu <hongbinqu9@gmail.com>
2019-06-11 15:40:34 -0700

    unionfind and trie

---
+++ AdvancedAlgorithm(LintCode)/UnionfindAndTrie/201_SegmentTreeBuild.java
@@ -0,0 +1,36 @@
+/**
+ * Definition of SegmentTreeNode:
+ * public class SegmentTreeNode {
+ *     public int start, end;
+ *     public SegmentTreeNode left, right;
+ *     public SegmentTreeNode(int start, int end) {
+ *         this.start = start, this.end = end;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+
+public class Solution {
+    /*
+     * @param start: start value.
+     * @param end: end value.
+     * @return: The root of Segment Tree.
+     */
+    public SegmentTreeNode build(int start, int end) {
+        if (start > end) {
+            return null;
+        }
+        
+        if (start == end) {
+            return new SegmentTreeNode(start, end);
+        }
+        
+        SegmentTreeNode root = new SegmentTreeNode(start, end);
+        int mid = start + (end - start) / 2;
+        root.left = build(start, mid);
+        root.right = build(mid + 1, end);
+        
+        return root;
+    }
+}
