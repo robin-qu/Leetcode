3e1e485c71c0949686b36e1f6cd38445ad213c75
robin-qu <hongbinqu9@gmail.com>
2019-06-18 15:12:23 -0700

    unionfind and trie

--- AdvancedAlgorithm(LintCode)/UnionfindAndTrie/473_AddAndSearchWordDataStructureDesign.java
+++ AdvancedAlgorithm(LintCode)/UnionfindAndTrie/473_AddAndSearchWordDataStructureDesign.java
@@ -1,4 +1,37 @@
+public class WordDictionary {
+    /*
+     * @param word: Adds a word into the data structure.
+     * @return: nothing
+     */
+    class TrieNode {
+        public TrieNode[] sons;
+        public boolean isWord;
+        
+        public TrieNode(TrieNode[] sons, boolean isWord) {
+            this.sons = sons;
+            this.isWord = isWord;
+        }
+    }
+    
+    private TrieNode root = new TrieNode(new TrieNode[26], false);
+    
+    public void addWord(String word) {
+        TrieNode curr = root;
+        
+        for (int i = 0; i < word.length(); i++) {
+            if (curr.sons[word.charAt(i) - 'a'] == null) {
+                curr.sons[word.charAt(i) - 'a'] = new TrieNode(new TrieNode[26], false);
+            }
+            curr = curr.sons[word.charAt(i) - 'a'];
+        }
+        curr.isWord = true;
+    }
 
+    /*
+     * @param word: A word could contain the dot character '.' to represent any one letter.
+     * @return: if the word is in the data structure.
+     */
+    public boolean search(String word) {
         return find(word, 0, root);
     }
     
@@ -23,3 +56,5 @@
             
             return find(word, start + 1, curr.sons[word.charAt(start) - 'a']);
         }
+    }
+} 
