9653ced3951379120cf5d0314212c51b67e0c334
robin-qu <hongbinqu9@gmail.com>
2019-06-19 13:07:25 -0700

    unionfind and trie

---
+++ AdvancedAlgorithm(LintCode)/UnionfindAndTrie/559_TrieService.java
@@ -0,0 +1,48 @@
+/**
+ * Definition of TrieNode:
+ * public class TrieNode {
+ *     public NavigableMap<Character, TrieNode> children;
+ *     public List<Integer> top10;
+ *     public TrieNode() {
+ *         children = new TreeMap<Character, TrieNode>();
+ *         top10 = new ArrayList<Integer>();
+ *     }
+ * }
+ */
+public class TrieService {
+
+    private TrieNode root = null;
+
+    public TrieService() {
+        root = new TrieNode();
+    }
+
+    public TrieNode getRoot() {
+        // Return root of trie root, and 
+        // lintcode will print the tree struct.
+        return root;
+    }
+
+    // @param word a string
+    // @param frequency an integer
+    public void insert(String word, int frequency) {
+        if (word == null || word.length() == 0) {
+            return;
+        }
+        
+        TrieNode curr = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (!curr.children.containsKey(c)) {
+                curr.children.put(c, new TrieNode());
+            }
+            curr = curr.children.get(c);
+            curr.top10.add(frequency);
+            Collections.sort(curr.top10);
+            Collections.reverse(curr.top10);
+            if (curr.top10.size() > 10) {
+                curr.top10.remove(curr.top10.size() - 1);
+            }
+        }
+    }
+ }
