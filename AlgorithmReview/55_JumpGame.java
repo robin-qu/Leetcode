e002a84c8b67ec3951b46cfdd27b693668c7568f
robin-qu <hongbinqu9@gmail.com>
2019-07-02 09:57:36 -0700

    algorithm review

--- AlgorithmReview/55_JumpGame.java
+++ AlgorithmReview/55_JumpGame.java
@@ -1,3 +1,30 @@
+// class Solution {
+//     /**
+//      * @param A: A list of integers
+//      * @return: A boolean
+//      */
+//     public boolean canJump(int[] A) {
+//         if (A == null || A.length == 0) {
+//             return false;
+//         }
+        
+//         int n = A.length;
+//         int[] dp = new int[n];
+//         dp[0] = 1;
+        
+//         for (int i = 1; i < n; i++) {
+//             for (int j = 0; j < i; j++) {
+//                 if (dp[j] > 0 && A[j] >= i - j) {
+//                     dp[i]++;
+//                 }
+//             }
+//         }
+        
+//         return dp[n - 1] > 0;
+//     }
+// }
+
+// Greedy
 class Solution {
     /**
      * @param A: A list of integers
@@ -9,17 +36,14 @@ class Solution {
         }
         
         int n = A.length;
-        int[] dp = new int[n];
-        dp[0] = 1;
         
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (dp[j] > 0 && A[j] >= i - j) {
-                    dp[i]++;
-                }
+        int farthest = A[0];
+        for (int i = 1; i < A.length; i++) {
+            if (i <= farthest) {
+                farthest = Math.max(farthest, i + A[i]);
             }
         }
         
-        return dp[n - 1] > 0;
+        return farthest >= n - 1;
     }
 }
