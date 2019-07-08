e97bb292cf6ae6001d70b2b86daf2adf2dad70d8
robin-qu <hongbinqu9@gmail.com>
2019-07-07 17:23:02 -0700

    dynamic programming I

---
+++ AdvancedAlgorithm(LintCode)/DynamicProgrammingI/150_BestTimeToBuyAndSellStockII.java
@@ -0,0 +1,23 @@
+// O(n)
+public class Solution {
+    /**
+     * @param prices: Given an integer array
+     * @return: Maximum profit
+     */
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+        
+        int n = prices.length;
+        
+        int maxProfit = 0;
+        for (int i = 1; i < n; i++) {
+            if (prices[i] > prices[i - 1]) {
+                maxProfit += prices[i] - prices[i - 1];
+            }
+        }
+        
+        return maxProfit;
+    }
+}
