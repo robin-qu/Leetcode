fac0d391f2c45ee24c1577a72f0b034e50e4a3a4
robin-qu <hongbinqu9@gmail.com>
2019-07-12 10:38:02 -0700

    dynamic programming II

--- AdvancedAlgorithm(LintCode)/DynamicProgrammingII/125_BackpackII.java
+++ AdvancedAlgorithm(LintCode)/DynamicProgrammingII/125_BackpackII.java
@@ -19,25 +19,16 @@
 //         // initialization
 //         dp[0][0] = 0;  // the first 0 item can fill size 0, with value 0
 //         for (int i = 1; i <= n; i++) {
-//             dp[i][0] = -1;  // the first i items cannot fill size 0
-//         }
-//         for (int i = 0; i <= n; i++) {
-//             for (int j = 1; j <= m; j++) {
-//                 dp[i][j] = -1;
-//             }
+//             dp[0][i] = -1;  // the first i items cannot fill size 0
 //         }
         
 //         int max = 0;
 //         for (int i = 1; i <= n; i++) {
-//             for (int j = 1; j <= m; j++) {
-//                 if (j < A[i - 1]) { // cannot put curretn item into the backpack
-//                     dp[i][j] = dp[i - 1][j];
-//                 } else {
-//                     if (dp[i - 1][j - A[i - 1]] > -1) {
-//                         dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - A[i - 1]] + V[i - 1]);
-//                     } else {  // dp[i - 1][j - A[i - 1]] == -1
-//                         dp[i][j] = Math.max(dp[i - 1][j], V[i - 1]);
-//                     }
+//             for (int j = 0; j <= m; j++) {
+//                 dp[i][j] = dp[i - 1][j];  // items i does not go into backpack
+                
+//                 if (j >= A[i - 1] && dp[i - 1][j - A[i - 1]] != -1) {   /// items i can go into backpack
+//                     dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - A[i - 1]] + V[i - 1]);
 //                 }
                 
 //                 max = Math.max(max, dp[i][j]);
@@ -49,8 +40,53 @@
 // }
 
 
+// // DP O(m) space rolling array
+// public class Solution {
+//     /**
+//      * @param m: An integer m denotes the size of a backpack
+//      * @param A: Given n items with size A[i]
+//      * @param V: Given n items with value V[i]
+//      * @return: The maximum value
+//      */
+//     public int backPackII(int m, int[] A, int[] V) {
+//         if (A == null || A.length == 0 ||
+//             V == null || V.length == 0 || m <= 0) {
+//             return 0;
+//         }
+        
+//         int n = A.length;
+        
+//         int[][] dp = new int[2][m + 1];  // dp[i][j] represents the maximum value of the first n items that can fill size j of the backpack, -1 if cannot fill
+        
+//         // initialization
+//         dp[0][0] = 0;  // the first 0 item can fill size 0, with value 0
+//         for (int i = 1; i <= m; i++) {
+//             dp[0][i] = -1;
+//         }
+        
+//         int max = 0;
+//         int old = 1;
+//         int now = 0;
+//         for (int i = 1; i <= n; i++) {
+//             old = now;
+//             now = 1 - now;
+//             for (int j = 0; j <= m; j++) {
+//                 dp[now][j] = dp[old][j]; // item i does not go into the backpack
+                
+//                 if (j >= A[i - 1] && dp[old][j - A[i - 1]] != -1) {
+//                     dp[now][j] = Math.max(dp[now][j], dp[old][j - A[i - 1]] + V[i - 1]);
+//                 }
+                
+//                 max = Math.max(max, dp[now][j]);
+//             }
+//         }
+        
+//         return max;
+//     }
+// }
 
-// DP O(m) space
+
+// 1D array DP
 public class Solution {
     /**
      * @param m: An integer m denotes the size of a backpack
@@ -66,34 +102,22 @@ public class Solution {
         
         int n = A.length;
         
-        int[][] dp = new int[2][m + 1];  // dp[i][j] represents the maximum value of the first n items that can fill size j of the backpack, -1 if cannot fill
+        int[] dp = new int[m + 1];  // dp[i][j] represents the maximum value of the first n items that can fill size j of the backpack, -1 if cannot fill
         
         // initialization
-        dp[0][0] = 0;  // the first 0 item can fill size 0, with value 0
-        dp[1][0] = -1;  // the first i items cannot fill size 0
+        dp[0] = 0;  // the first 0 item can fill size 0, with value 0
         for (int i = 1; i <= m; i++) {
-            dp[0][i] = -1;
-            dp[1][i] = -1;
+            dp[i] = -1;
         }
         
         int max = 0;
-        int old = 1;
-        int now = 0;
         for (int i = 1; i <= n; i++) {
-            now = old;
-            old = 1 - now;
-            for (int j = 1; j <= m; j++) {
-                if (j < A[i - 1]) { // cannot put curretn item into the backpack
-                    dp[now][j] = dp[old][j];
-                } else {
-                    if (dp[old][j - A[i - 1]] > -1) {
-                        dp[now][j] = Math.max(dp[old][j], dp[old][j - A[i - 1]] + V[i - 1]);
-                    } else {  // dp[i - 1][j - A[i - 1]] == -1
-                        dp[now][j] = Math.max(dp[old][j], V[i - 1]);
-                    }
+            for (int j = m; j >= A[i - 1]; j--) {
+                if (dp[j - A[i - 1]] != -1) {
+                    dp[j] = Math.max(dp[j], dp[j - A[i - 1]] + V[i - 1]);
                 }
                 
-                max = Math.max(max, dp[now][j]);
+                max = Math.max(max, dp[j]);
             }
         }
         
