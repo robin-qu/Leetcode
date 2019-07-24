2b26aa60e261f643681821d80c97b40cc8161cdd
robin-qu <hongbinqu9@gmail.com>
2019-07-24 12:14:58 -0700

    JZOffer

---
+++ LintCode-JianZhiOffer/9_FizzBuzz.java
@@ -0,0 +1,23 @@
+public class Solution {
+    /**
+     * @param n: An integer
+     * @return: A list of strings.
+     */
+    public List<String> fizzBuzz(int n) {
+        List<String> res = new ArrayList<>();
+        
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                res.add("fizz buzz");
+            } else if (i % 3 == 0) {
+                res.add("fizz");
+            } else if (i % 5 == 0) {
+                res.add("buzz");
+            } else {
+                res.add(String.valueOf(i));
+            }
+        }
+        
+        return res;
+    }
+}
