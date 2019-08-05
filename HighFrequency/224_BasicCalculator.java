386872bc4fa61703c4cd8cb273701910bf91160a
robin-qu <hongbinqu9@gmail.com>
2019-08-04 17:56:03 -0700

    high frequency

--- HighFrequency/224_BasicCalculator.java
+++ HighFrequency/224_BasicCalculator.java
@@ -1,3 +1,58 @@
+// // Stack O(n)time O(n)space
+// class Solution {
+//     public int calculate(String s) {
+//         if (s == null || s.length() == 0) {
+//             return 0;
+//         }
+        
+//         int n = s.length();
+//         Stack<Object> stack = new Stack<>();
+//         int num = 0;
+        
+//         for (int i = 0; i < n; i++) {
+//             char c = s.charAt(i);
+            
+//             if (c == ' ') {
+//                 continue;
+//             } else if (c == '(') {
+//                 stack.push(c);
+//             } else if (c == ')') {
+//                 int temp = (Integer) stack.pop();
+//                 stack.pop(); // pop '('
+//                 evaluate(stack, temp);
+//             } else if (Character.isDigit(c)) {
+//                 num = c - '0';
+//                 while (i + 1 < n && Character.isDigit(s.charAt(i + 1))) {  // extract the number
+//                     i++;
+//                     num = num * 10 + s.charAt(i) - '0';
+//                 }
+                
+//                 evaluate(stack, num);
+//                 num = 0;
+//             } else {  // c is operator
+//                 stack.push(c);
+//             }
+//         }
+        
+//         return (Integer) stack.peek();
+//     }
+    
+//     private void evaluate(Stack<Object> stack, int num) {
+//         if (stack.isEmpty()) {
+//             stack.push(num);
+//         } else if ((stack.peek() instanceof Character) && ((Character) stack.peek()) == '+') {
+//             stack.pop();
+//             stack.push((Integer) stack.pop() + num);
+//         } else if ((stack.peek() instanceof Character) && ((Character) stack.peek()) == '-') {
+//             stack.pop();
+//             stack.push((Integer) stack.pop() - num);
+//         } else {
+//             stack.push(num);
+//         }
+//     }
+// }
+
+
 // Stack O(n)time O(n)space
 class Solution {
     public int calculate(String s) {
@@ -6,48 +61,40 @@ class Solution {
         }
         
         int n = s.length();
-        Stack<Object> stack = new Stack<>();
-        int num = 0;
+        char[] ss = s.toCharArray();
+        Stack<Integer> stack = new Stack<>();
+        int sign = 1;
+        int res = 0;
         
         for (int i = 0; i < n; i++) {
-            char c = s.charAt(i);
+            char c = ss[i];
             
             if (c == ' ') {
                 continue;
-            } else if (c == '(') {
-                stack.push(c);
-            } else if (c == ')') {
-                int temp = (Integer) stack.pop();
-                stack.pop(); // pop '('
-                evaluate(stack, temp);
-            } else if (Character.isDigit(c)) {
-                num = c - '0';
-                while (i + 1 < n && Character.isDigit(s.charAt(i + 1))) {  // extract the number
+            }
+            
+            if (Character.isDigit(c)) {
+                int num = c - '0';
+                while (i + 1 < n && Character.isDigit(s.charAt(i + 1))) {
                     i++;
                     num = num * 10 + s.charAt(i) - '0';
                 }
                 
-                evaluate(stack, num);
-                num = 0;
-            } else {  // c is operator
-                stack.push(c);
+                res += sign * num;
+            } else if (c == '(') {
+                stack.push(res);
+                stack.push(sign);
+                // begin a new expression, reset res and sign
+                res = 0;
+                sign = 1;
+            } else if (c == ')') {
+                res = stack.pop() * res;  // sign
+                res += stack.pop(); // previous result
+            } else {  // operator
+                sign = (c == '+' ? 1 : -1);
             }
         }
         
-        return (Integer) stack.peek();
-    }
-    
-    private void evaluate(Stack<Object> stack, int num) {
-        if (stack.isEmpty()) {
-            stack.push(num);
-        } else if ((stack.peek() instanceof Character) && ((Character) stack.peek()) == '+') {
-            stack.pop();
-            stack.push((Integer) stack.pop() + num);
-        } else if ((stack.peek() instanceof Character) && ((Character) stack.peek()) == '-') {
-            stack.pop();
-            stack.push((Integer) stack.pop() - num);
-        } else {
-            stack.push(num);
-        }
+        return res;
     }
 }
---
+++ HighFrequency/227_BasicCalculatorII.java
@@ -0,0 +1,100 @@
+// // stack O(n)time O(n)space
+// class Solution {
+//     public int calculate(String s) {
+//         if (s == null || s.length() == 0) {
+//             return 0;
+//         }
+        
+//         int n = s.length();
+//         char[] ss = s.toCharArray();
+//         Stack<Integer> stack = new Stack<>();
+//         char sign = '+';
+        
+//         for (int i = 0; i < n; i++) {
+//             char c = ss[i];
+            
+//             if (Character.isDigit(c)) {
+//                 num = ss[i] - '0';
+//                 while (i + 1 < n && Character.isDigit(ss[i + 1])) {
+//                     num = num * 10 + ss[i + 1] - '0';
+//                     i++;
+//                 }
+                
+//                 if (sign == '+') {
+//                     stack.push(num);
+//                 } else if (sign == '-') {
+//                     stack.push(-num);
+//                 } else if (sign == '*') {
+//                     stack.push(stack.pop() * num);
+//                 } else if (sign == '/') {
+//                     stack.push(stack.pop() / num);
+//                 }
+//                 num = 0;
+//                 continue;
+//             }
+            
+//             if (c == ' ') {
+//                 continue;
+//             }
+            
+//             sign = c;
+//         }
+        
+//         int res = 0;
+//         while (!stack.isEmpty()) {
+//             res += stack.pop();
+//         }
+        
+//         return res;
+//     }
+// }
+
+
+// stack O(n)time O(1)space
+class Solution {
+    public int calculate(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        
+        int n = s.length();
+        char[] ss = s.toCharArray();
+        char sign = '+';
+        long res = 0;
+        long prev = 0;
+        
+        for (int i = 0; i < n; i++) {
+            char c = ss[i];
+            
+            if (c == ' ') {
+                continue;
+            }
+            
+            if (Character.isDigit(c)) {
+                int num = ss[i] - '0';
+                while (i + 1 < n && Character.isDigit(ss[i + 1])) {
+                    num = num * 10 + ss[i + 1] - '0';
+                    i++;
+                }
+                
+                if (sign == '+') {
+                    res += prev;
+                    prev = num;
+                } else if (sign == '-') {
+                    res += prev;
+                    prev = -num;
+                } else if (sign == '*') {
+                    prev *= num;
+                } else if (sign == '/') {
+                    prev /= num;
+                }
+                
+                num = 0;
+            }
+            
+            sign = c;
+        }
+        
+        return (int) res + (int) prev;
+    }
+}
