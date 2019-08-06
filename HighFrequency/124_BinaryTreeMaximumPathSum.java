4676f17aaafd10b8f932a85048e49a90a9411f5a
robin-qu <hongbinqu9@gmail.com>
2019-08-06 10:05:04 -0700

    color game project

---
+++ HighFrequency/124_BinaryTreeMaximumPathSum.java
@@ -0,0 +1,65 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+// // Recurssion without global variable O(2^n)time
+// class Solution {
+//     public int maxPathSum(TreeNode root) {
+//         if (root == null) {
+//             return Integer.MIN_VALUE;
+//         }
+        
+//         int leftBranch = Math.max(maxFromRoot(root.left), 0);
+//         int rightBranch = Math.max(maxFromRoot(root.right), 0);
+        
+//         int left = maxPathSum(root.left);
+//         int right = maxPathSum(root.right);
+        
+//         return Math.max(root.val + leftBranch + rightBranch, Math.max(left, right));
+//     }
+    
+//     private int maxFromRoot(TreeNode root) {
+//         if (root == null) {
+//             return 0;
+//         }
+        
+//         int left = Math.max(maxFromRoot(root.left), 0);
+//         int right = Math.max(maxFromRoot(root.right), 0);
+        
+//         return root.val + Math.max(left, right);
+//     }
+// }
+
+
+// Recurssion with global variable O(n)time
+class Solution {
+    private int max = Integer.MIN_VALUE;
+    
+    public int maxPathSum(TreeNode root) {
+        if (root == null) {
+            return Integer.MIN_VALUE;
+        }
+        
+        maxFromRoot(root);
+        return max;
+    }
+    
+    private int maxFromRoot(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        
+        int left = Math.max(maxFromRoot(root.left), 0);
+        int right = Math.max(maxFromRoot(root.right), 0);
+        
+        max = Math.max(max, root.val + left + right);
+        
+        return root.val + Math.max(left, right);
+    }
+}
