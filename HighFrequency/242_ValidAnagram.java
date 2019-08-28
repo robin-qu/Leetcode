b643aecf5898e4ba54e0b31cc8b89830bbf59864
robin-qu <hongbinqu9@gmail.com>
2019-08-28 10:51:11 -0700

    high frequency

---
+++ HighFrequency/242_ValidAnagram.java
@@ -0,0 +1,24 @@
+// O(n)time O(1)space
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s == null || t == null || s.length() != t.length()) {
+            return false;
+        }
+        
+        int n = s.length();
+        int[] count = new int[26];
+        
+        for (int i = 0; i < n; i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+        
+        for (int c : count) {
+            if (c != 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
