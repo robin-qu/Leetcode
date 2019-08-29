abad6bc910a73a8db765708bf4cacfd6f4f3b846
robin-qu <hongbinqu9@gmail.com>
2019-08-29 10:49:28 -0700

    high frequency

--- HighFrequency/347_TopKFrequentElements.java
+++ HighFrequency/347_TopKFrequentElements.java
@@ -49,6 +49,56 @@
 
 
 
+// // Bucketsort O(n)time O(n)space
+// class Solution {    
+//     public List<Integer> topKFrequent(int[] nums, int k) {
+//         if (nums == null || nums.length == 0) {
+//             return new ArrayList<>();
+//         }
+        
+//         Map<Integer, Integer> count = new HashMap<>();
+//         for (int num : nums) {
+//             if (!count.containsKey(num)) {
+//                 count.put(num, 1);
+//             } else {
+//                 count.put(num, count.get(num) + 1);
+//             }
+//         }
+        
+//         int n = nums.length;
+//         List<Integer>[] bucket = new List[n + 1];  // frequency varies from 0 to n
+        
+//         for (int key : count.keySet()) {
+//             int freq = count.get(key);
+            
+//             if (bucket[freq] == null) {
+//                 bucket[freq] = new ArrayList<>();
+//             }
+//             bucket[freq].add(key);
+//         }
+        
+//         List<Integer> res = new ArrayList<>();
+//         for (int i = n; i >= 0; i--) {
+//             List<Integer> list = bucket[i];
+//             if (list == null) {
+//                 continue;
+//             }
+            
+//             if (res.size() + list.size() <= k) {
+//                 res.addAll(list);
+//             } else {
+//                 for (int j = 0; j < Math.min(list.size(), k - res.size()); j++) {
+//                     res.add(list.get(j));
+//                 }
+//             }
+//         }
+        
+//         return res;
+//     }
+// }
+
+
+
 // Bucketsort O(n)time O(n)space
 class Solution {    
     public List<Integer> topKFrequent(int[] nums, int k) {
@@ -56,40 +106,27 @@ class Solution {
             return new ArrayList<>();
         }
         
-        Map<Integer, Integer> count = new HashMap<>();
+        int n = nums.length;
+        List<Integer>[] bucket = new List[n + 1];
+        Map<Integer, Integer> count = new HashMap<>();  // num -> frequence
+        
         for (int num : nums) {
-            if (!count.containsKey(num)) {
-                count.put(num, 1);
-            } else {
-                count.put(num, count.get(num) + 1);
-            }
+            count.put(num, count.getOrDefault(num, 0) + 1);
         }
         
-        int n = nums.length;
-        List<Integer>[] bucket = new List[n + 1];  // frequency varies from 0 to n
-        
-        for (int key : count.keySet()) {
-            int freq = count.get(key);
-            
-            if (bucket[freq] == null) {
-                bucket[freq] = new ArrayList<>();
+        for (int num : count.keySet()) {
+            int idx = count.get(num);
+            if (bucket[idx] == null) {
+                bucket[idx] = new ArrayList<>();
             }
-            bucket[freq].add(key);
+            
+            bucket[idx].add(num);
         }
         
         List<Integer> res = new ArrayList<>();
-        for (int i = n; i >= 0; i--) {
-            List<Integer> list = bucket[i];
-            if (list == null) {
-                continue;
-            }
-            
-            if (res.size() + list.size() <= k) {
-                res.addAll(list);
-            } else {
-                for (int j = 0; j < Math.min(list.size(), k - res.size()); j++) {
-                    res.add(list.get(j));
-                }
+        for (int i = n; i >= 0 && res.size() < k; i--) {
+            if (bucket[i] != null) {
+                res.addAll(bucket[i]);
             }
         }
         
