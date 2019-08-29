5129831d34d8e2f3cc7344d2a1490236dbead67f
robin-qu <hongbinqu9@gmail.com>
2019-08-29 11:00:20 -0700

    high frequency

--- HighFrequency/692_TopKFrequentWords.java
+++ HighFrequency/692_TopKFrequentWords.java
@@ -1,49 +1,93 @@
-// heap  O(nlogk)time O(n)space
-class Solution {
-    class Word implements Comparable<Word> {
-        public String word;
-        public int freq;
+// // heap  O(nlogk)time O(n)space
+// class Solution {
+//     class Word implements Comparable<Word> {
+//         public String word;
+//         public int freq;
         
-        public Word(String word, int freq) {
-            this.word = word;
-            this.freq = freq;
-        }
+//         public Word(String word, int freq) {
+//             this.word = word;
+//             this.freq = freq;
+//         }
         
-        @Override
-        public int compareTo(Word other) {
-            if (this.freq != other.freq) {
-                return this.freq - other.freq;
-            }
+//         @Override
+//         public int compareTo(Word other) {
+//             if (this.freq != other.freq) {
+//                 return this.freq - other.freq;
+//             }
             
-            return other.word.compareTo(this.word);
-        }
-    }
+//             return other.word.compareTo(this.word);
+//         }
+//     }
     
+//     public List<String> topKFrequent(String[] words, int k) {
+//         if (words == null || words.length == 0 || k < 1) {
+//             return new ArrayList<>();
+//         }
+        
+//         Map<String, Integer> counts = new HashMap<>();
+//         for (String word : words) {
+//             if (!counts.containsKey(word)) {
+//                 counts.put(word, 1);
+//             } else {
+//                 counts.put(word, counts.get(word) + 1);
+//             }
+//         }
+        
+//         PriorityQueue<Word> pq = new PriorityQueue<>();
+//         for (String key : counts.keySet()) {
+//             pq.offer(new Word(key, counts.get(key)));
+//             if (pq.size() > k) {
+//                 pq.poll();
+//             }
+//         }
+        
+//         List<String> res = new ArrayList<>();
+//         while (!pq.isEmpty()) {
+//             res.add(0, pq.poll().word);
+//         }
+        
+//         return res;
+//     }
+// }
+
+
+// heap  O(nlogk)time O(n)space
+class Solution {    
     public List<String> topKFrequent(String[] words, int k) {
         if (words == null || words.length == 0 || k < 1) {
             return new ArrayList<>();
         }
         
-        Map<String, Integer> counts = new HashMap<>();
+        int n = words.length;
+        Map<String, Integer> count = new HashMap<>(); // word -> frequency
+        
         for (String word : words) {
-            if (!counts.containsKey(word)) {
-                counts.put(word, 1);
-            } else {
-                counts.put(word, counts.get(word) + 1);
-            }
+            count.put(word, count.getOrDefault(word, 0) + 1);
         }
         
-        PriorityQueue<Word> pq = new PriorityQueue<>();
-        for (String key : counts.keySet()) {
-            pq.offer(new Word(key, counts.get(key)));
+        PriorityQueue<String> pq = new PriorityQueue<>(new Comparator<String>() {
+            @Override
+            public int compare(String a, String b) {
+                if (count.get(a) != count.get(b)) {
+                    return count.get(a) - count.get(b);
+                }
+                
+                return b.compareTo(a);
+            }
+        });
+        
+        for (String word : count.keySet()) {
+            pq.offer(word);
+            
             if (pq.size() > k) {
                 pq.poll();
             }
         }
         
         List<String> res = new ArrayList<>();
+        
         while (!pq.isEmpty()) {
-            res.add(0, pq.poll().word);
+            res.add(0, pq.poll());
         }
         
         return res;
