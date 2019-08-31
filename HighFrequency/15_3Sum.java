c2890b3cb89e34559fc00ee67bcd40ea19a81ad5
robin-qu <hongbinqu9@gmail.com>
2019-08-31 14:26:06 -0700

    high frequency

--- HighFrequency/15_3Sum.java
+++ HighFrequency/15_3Sum.java
@@ -1,53 +1,52 @@
-// Two pointers O(n^2)time O(1)space
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        if (nums == null || nums.length < 3) {
+        if (nums == null || nums.length == 0) {
             return new ArrayList<>();
         }
         
         int n = nums.length;
         Arrays.sort(nums);
         List<List<Integer>> res = new ArrayList<>();
-        List<Integer> comb = new ArrayList<>();
         
         for (int i = 0; i < n - 2; i++) {
             if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;  // get rid of duplicates
+                continue;
             }
             
-            if (nums[i] > 0) {
-                break;  // since nums is sorted, we cannnot find three positive numbers whose sum is equal to zero
-            }
-            
-            twoSum(nums, i + 1, -nums[i], res);
+            twoSum(i, nums, -nums[i], res);
         }
         
         return res;
     }
     
-    private void twoSum(int[] nums, int idx, int target, List<List<Integer>> res) {
-        int left = idx;
+    private void twoSum(int idx, int[] nums, int target, List<List<Integer>> res) {
+        int left = idx + 1;
         int right = nums.length - 1;
         
         while (left < right) {
-            if (nums[left] + nums[right] < target) {
+            while (left < right && nums[left] + nums[right] < target) {
                 left++;
-            } else if (nums[left] + nums[right] > target) {
+            }
+            
+            while (left < right && nums[left] + nums[right] > target) {
                 right--;
-            } else {
-                List<Integer> comb = new ArrayList<>();
-                comb.add(-target);
-                comb.add(nums[left]);
-                comb.add(nums[right]);
-                res.add(comb);
-                left++;
-                right--;
+            }
+            
+            if (left < right && nums[left] + nums[right] == target) {
+                List<Integer> list = new ArrayList<>();
+                list.add(-target);
+                list.add(nums[left]);
+                list.add(nums[right]);
+                res.add(list);
                 
+                left++;
                 while (left < right && nums[left] == nums[left - 1]) {
-                    left++;  // remove duplicates
+                    left++;
                 }
+                
+                right--;
                 while (left < right && nums[right] == nums[right + 1]) {
-                    right--;  // remove duplicates
+                    right--;
                 }
             }
         }
