ba7031ee028bbd970a67c111bb6a4cf218da81a5
robin-qu <hongbinqu9@gmail.com>
2019-10-01 16:59:50 -0700

    high frequency

---
+++ HighFrequency/714_BestTimeToBuyAndSellStockWithTransactionFee.java
@@ -0,0 +1,20 @@
+// O(n)time O(1)space
+class Solution {
+    public int maxProfit(int[] prices, int fee) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+        
+        int n = prices.length;
+        
+        int buy = -prices[0];
+        int sell = 0;
+        
+        for (int i = 1; i < n; i++) {
+            buy = Math.max(buy, sell - prices[i]);
+            sell = Math.max(sell, buy + prices[i] - fee);
+        }
+        
+        return sell;
+    }
+}
