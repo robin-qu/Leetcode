3e4dd67c98f02f17dc7546d62e6e31ec9463240c
robin-qu <hongbinqu9@gmail.com>
2019-10-01 16:18:24 -0700

    high frequency

---
+++ HighFrequency/122_BestTimeToBuyAndSellStockII.java
@@ -0,0 +1,19 @@
+// O(n)time O(1)space
+class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+        
+        int n = prices.length;
+        int res = 0;
+        
+        for (int i = 1; i < n; i++) {
+            if (prices[i] > prices[i - 1]) {
+                res += prices[i] - prices[i - 1];
+            }
+        }
+        
+        return res;
+    }
+}
