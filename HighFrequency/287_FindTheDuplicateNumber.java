f433406b0b8161c8e0743158334ebd9347b8c970
robin-qu <hongbinqu9@gmail.com>
2019-10-04 14:38:26 -0700

    high frequency

--- HighFrequency/287_FindTheDuplicateNumber.java
+++ HighFrequency/287_FindTheDuplicateNumber.java
@@ -1,102 +1,70 @@
-// // O(nlogn)time O(n)space
-// class Solution {    
-//     public String reorganizeString(String s) {
-//         if (s == null || s.length() <= 1) {
-//             return s;
+// // BinarySearch O(nlogn)time O(1)space
+// class Solution {
+//     public int findDuplicate(int[] nums) {
+//         if (nums == null || nums.length == 0) {
+//             return 0;
 //         }
         
-//         int n = s.length();
-//         Character[] ss = new Character[n];
-//         for (int i = 0; i < n; i++) {
-//             ss[i] = s.charAt(i);
-//         }
-//         Map<Character, Integer> counts = new HashMap<>();
+//         int n = nums.length;
         
-//         for (char c : ss) {
-//             counts.put(c, counts.getOrDefault(c, 0) + 1);
-//             if (counts.get(c) > (n + 1) / 2) {
-//                 return "";
+//         int low = 1;
+//         int high = n - 1;
+        
+//         while (low + 1 < high) {
+//             int mid = low + (high - low) / 2;
+//             int count = countLess(mid, nums);
+            
+//             if (count <= mid) {
+//                 low = mid;
+//             } else {
+//                 high = mid;
 //             }
 //         }
         
-//         Arrays.sort(ss, new Comparator<Character>() {
-//             @Override
-//             public int compare(Character a, Character b) {
-//                 if (counts.get(a) != counts.get(b)) {
-//                     return counts.get(b) - counts.get(a);
-//                 }
-//                 return a - b;
+//         if (countLess(low, nums) > low) {
+//             return low;
+//         }
+        
+//         return high;
+//     }
+    
+//     private int countLess(int a, int[] nums) {
+//         int count = 0;
+//         for (int num : nums) {
+//             if (num <= a) {
+//                 count++;
 //             }
-//         });
-        
-//         char[] res = new char[n];
-//         int idx = 0;
-//         for (int i = 0; i < n; i += 2) {
-//             res[i] = ss[idx++];
-//         }
-//         for (int i = 1; i < n; i += 2) {
-//             res[i] = ss[idx++];
 //         }
         
-//         return String.valueOf(res);
+//         return count;
 //     }
 // }
 
 
-// pq O(nlogn)time O(n)space
-class Solution {    
-    public String reorganizeString(String s) {
-        if (s == null || s.length() <= 1) {
-            return s;
+// Two Pointers O(n)time O(1)space
+class Solution {
+    public int findDuplicate(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
         }
         
-        int n = s.length();
-        Map<Character, Integer> counts = new HashMap<>();
+        int n = nums.length;
         
-        for (char c : s.toCharArray()) {
-            counts.put(c, counts.getOrDefault(c, 0) + 1);
+        int slow = nums[0];
+        int fast = nums[0];
+        
+        while (nums[slow] != nums[nums[fast]]) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
         }
         
-        PriorityQueue<int[]> pq = new PriorityQueue<int[]>(new Comparator<int[]>() {
-            @Override
-            public int compare(int[] a, int[] b) {
-                if (a[1] != b[1]) {
-                    return b[1] - a[1];
-                }
-                
-                return a[0] - b[0];
-            }
-        });
+        int p = 0;
         
-        for (char key : counts.keySet()) {
-            if (counts.get(key) > (n + 1) / 2) {
-                return "";
-            }
-            pq.offer(new int[]{key, counts.get(key)});
+        while (nums[p] != nums[slow]) {
+            slow = nums[slow];
+            p = nums[p];
         }
         
-        StringBuilder sb = new StringBuilder();
-        
-        while (!pq.isEmpty()) {
-            int[] first = pq.poll();
-            
-            if (sb.length() == 0 || sb.charAt(sb.length() - 1) != first[0]) {
-                sb.append((char) first[0]);
-                first[1]--;
-            } else {
-                int[] second = pq.poll();
-                sb.append((char) second[0]);
-                second[1]--;
-                if (second[1] > 0) {
-                    pq.offer(second);
-                }
-            }
-            
-            if (first[1] > 0) {
-                pq.offer(first);
-            }
-        }
-        
-        return sb.toString();
+        return nums[p];
     }
 }
