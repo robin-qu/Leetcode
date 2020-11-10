f91e8d6cd667d7a61eba594afd81388026a9aa9b
robin-qu <hongbinqu9@gmail.com>
2020-11-10 13:09:24 +0800

    LC CN

---
+++ PremiumCN/53_MaximumSubarray.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        for (int num : nums) {
+            sum += num;
+            max = Math.max(max, sum);
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return max;
+    }
+}
