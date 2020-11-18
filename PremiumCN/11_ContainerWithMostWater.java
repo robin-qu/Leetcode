043389e45e607b2bdb03285638e41ee437d9e789
robin-qu <hongbinqu9@gmail.com>
2020-11-18 13:19:02 +0800

    LC CN

---
+++ PremiumCN/11_ContainerWithMostWater.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxArea(int[] height) {
+        if (height == null || height.length <= 1) {
+            return 0;
+        }
+        
+        int n = height.length;
+        
+        int left = 0;
+        int right = n - 1;
+        int res = 0;
+        while (left < right) {
+            res = Math.max(res, (right - left) * Math.min(height[left], height[right]));
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        
+        return res;
+    }
+}
