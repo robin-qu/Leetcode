6378c8fac46b4de189f416565c7ffc50570bccdf
robin-qu <hongbinqu9@gmail.com>
2020-12-05 13:58:27 +0800

    LC CN

---
+++ PremiumCN/239_SlidingWindowMaximum.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return new int[0];
+        }
+
+        int n = nums.length;
+        int[] res = new int[n - k + 1];
+        Deque<Integer> deque = new LinkedList<>();
+        for (int i = 0; i < k; i++) {
+            while (!deque.isEmpty() && nums[i] > deque.peekLast()) {
+                deque.pollLast();
+            }
+            deque.offerLast(nums[i]);
+        }
+        res[0] = deque.peekFirst();
+        for (int i = k; i < n; i++) {
+            if (nums[i - k] == deque.peekFirst()) {
+                deque.pollFirst();
+            }
+            while (!deque.isEmpty() && nums[i] > deque.peekLast()) {
+                deque.pollLast();
+            }
+            deque.offerLast(nums[i]);
+            res[i - k + 1] = deque.peekFirst();
+        }
+        return res;
+    }
+}
