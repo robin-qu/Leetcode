76d9877c5d721cafdad6cbcd0bc88552290fab54
robin-qu <hongbinqu9@gmail.com>
2020-12-19 13:59:56 +0800

    LC CN

---
+++ PremiumCN/123_BestTimeToBuyAndSellStockIII.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+
+        int n = prices.length;
+        int firstBuy = Integer.MIN_VALUE;
+        int firstSell = 0;
+        int secondBuy = Integer.MIN_VALUE;
+        int secondSell = 0;
+
+        for (int price : prices) {
+            firstBuy = Math.max(firstBuy, -price);
+            firstSell = Math.max(firstSell, firstBuy + price);
+            secondBuy = Math.max(secondBuy, firstSell - price);
+            secondSell = Math.max(secondSell, secondBuy + price);
+        }
+
+        return secondSell;
+    }
+}
