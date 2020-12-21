f0e3531ab349fd6cb426cc2212757f3e634bf0ff
robin-qu <hongbinqu9@gmail.com>
2020-12-21 13:42:11 +0800

    LC CN

---
+++ PremiumCN/387_FirstUniqueCharacterInAString.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        if (s == null || s.length() == 0) {
+            return -1;
+        }
+
+        int[] counts = new int[26];
+        for (char c : s.toCharArray()) {
+            counts[c - 'a']++;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (counts[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
