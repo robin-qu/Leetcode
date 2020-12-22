f5dbdf385bddbf36874ca2c792e6422fbb6565a4
robin-qu <hongbinqu9@gmail.com>
2020-12-22 13:01:26 +0800

    LC CN

---
+++ PremiumCN/150_EvaluateReversePolishNotation.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        if (tokens == null || tokens.length == 0) {
+            return 0;
+        }
+
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (String token : tokens) {
+            if ("+".equals(token)) {
+                stack.push(stack.pop() + stack.pop());
+            } else if ("-".equals(token)) {
+                stack.push(-stack.pop() + stack.pop());
+            } else if ("*".equals(token)) {
+                stack.push(stack.pop() * stack.pop());
+            } else if ("/".equals(token)) {
+                int a = stack.pop();
+                int b = stack.pop();
+                stack.push(b / a);
+            } else {
+                stack.push(Integer.parseInt(token));
+            }
+        }
+
+        return stack.pop();
+    }
+}
