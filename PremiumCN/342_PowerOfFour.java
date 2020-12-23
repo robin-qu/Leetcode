617240e80c2683ac3da01208089e7987df0e5696
robin-qu <hongbinqu9@gmail.com>
2020-12-23 21:59:11 +0800

    LC CN

---
+++ PremiumCN/342_PowerOfFour.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if (n <= 0) {
+            return false;
+        }
+
+        long x = (long) n;
+        return (x & (x - 1)) == 0 && (n - 1) % 3 == 0;
+    }
+}
