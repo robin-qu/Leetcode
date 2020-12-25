1af9407b861e4e2392333c74186ec3d010c1d203
robin-qu <hongbinqu9@gmail.com>
2020-12-25 13:26:46 +0800

    LC CN

---
+++ PremiumCN/LintCode439_SegmentTreeBuildII.java
@@ -0,0 +1,90 @@
+// /**
+//  * Definition of SegmentTreeNode:
+//  * public class SegmentTreeNode {
+//  *     public int start, end, max;
+//  *     public SegmentTreeNode left, right;
+//  *     public SegmentTreeNode(int start, int end, int max) {
+//  *         this.start = start;
+//  *         this.end = end;
+//  *         this.max = max
+//  *         this.left = this.right = null;
+//  *     }
+//  * }
+//  */
+
+// public class Solution {
+//     /**
+//      * @param A: a list of integer
+//      * @return: The root of Segment Tree
+//      */
+//     public SegmentTreeNode build(int[] A) {
+//         if (A == null || A.length == 0) {
+//             return null;
+//         }
+        
+//         return helper(A, 0, A.length - 1);
+//     }
+    
+//     private SegmentTreeNode helper(int[] A, int start, int end) {
+//         if (start == end) {
+//             return new SegmentTreeNode(start, end, A[start]);
+//         }
+        
+//         SegmentTreeNode root = new SegmentTreeNode(start, end, 0);
+//         int mid = start + (end - start) / 2;
+//         root.left = helper(A, start, mid);
+//         root.right = helper(A, mid + 1, end);
+//         root.max = Math.max(root.left.max, root.right.max);
+//         return root;
+//     }
+// }
+
+
+
+/**
+ * Definition of SegmentTreeNode:
+ * public class SegmentTreeNode {
+ *     public int start, end, max;
+ *     public SegmentTreeNode left, right;
+ *     public SegmentTreeNode(int start, int end, int max) {
+ *         this.start = start;
+ *         this.end = end;
+ *         this.max = max
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param A: a list of integer
+     * @return: The root of Segment Tree
+     */
+    public SegmentTreeNode build(int[] A) {
+        if (A == null || A.length == 0) {
+            return null;
+        }
+        
+        return helper(A, 0, A.length - 1);
+    }
+    
+    private SegmentTreeNode helper(int[] A, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+        
+        if (start == end) {
+            return new SegmentTreeNode(start, end, A[start]);
+        }
+        
+        int mid = start + (end - start) / 2;
+        SegmentTreeNode left = helper(A, start, mid);
+        SegmentTreeNode right = helper(A, mid + 1, end);
+        
+        SegmentTreeNode root = new SegmentTreeNode(start, end, Math.max(left.max, right.max));
+        root.left = left;
+        root.right = right;
+        
+        return root;
+    }
+}
