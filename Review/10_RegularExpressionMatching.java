30d9d6e358afc2519bf45865a3c92dedc2a032de
robin-qu <hongbinqu9@gmail.com>
2021-01-02 14:08:58 +0800

    review

---
+++ Review/10_RegularExpressionMatching.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        if (s == null || p == null) {
+            return false;
+        }
+
+        int m = s.length();
+        int n = p.length();
+        char[] ss = s.toCharArray();
+        char[] pp = p.toCharArray();
+
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+        for (int i = 0; i < n; i++) {
+            if (pp[i] == '*') {
+                dp[0][i + 1] = dp[0][i - 1];
+            }
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (pp[j - 1] == '.' || pp[j - 1] == ss[i - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (pp[j - 1] == '*') {
+                    dp[i][j] = dp[i][j - 2] || dp[i][j - 1]; // zero and one
+                    dp[i][j] = dp[i][j] || ((ss[i - 1] == pp[j - 2] || pp[j - 2] == '.') && dp[i - 1][j]);
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+}
